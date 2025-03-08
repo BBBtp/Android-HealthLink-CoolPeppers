1a429766dc55bcbce8370b812005d3f39c07a791
tkholmatov <kholmatov@student.bmstu.ru>
2025-03-08 11:47:52 +0300

    about, settings 08.03

--- app/src/main/java/com/CoolPeppers/android/Profile/ProfileScreen.kt
+++ app/src/main/java/com/CoolPeppers/android/Profile/ProfileScreen.kt
@@ -1,14 +1,10 @@
-import android.graphics.Color
-import android.provider.ContactsContract.Profile
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -18,24 +14,27 @@ import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Build
-import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.ripple.rememberRipple
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -67,11 +66,32 @@ fun OptionsList(modifier: Modifier = Modifier) {
             .fillMaxWidth()
             .padding(start = 16.dp, end = 8.dp)
     ) {
-        Option(Icons.Default.Face, "Редактирование профиля")
-        Option(Icons.Default.Build, "Настройки")
-        Option(Icons.Default.Refresh, "История записей")
-        Option(Icons.Default.Info, "О приложении")
-        Option(Icons.AutoMirrored.Filled.ExitToApp, "Выход")
+        Option(icon = Icons.Default.Face, text = "Редактирование профиля", buttonIcon = Icons.AutoMirrored.Filled.KeyboardArrowRight)
+        Option(icon = Icons.Default.Build, text = "Настройки", buttonIcon = Icons.AutoMirrored.Filled.KeyboardArrowRight)
+        Option(icon = Icons.Default.Refresh, text = "История записей", buttonIcon = Icons.AutoMirrored.Filled.KeyboardArrowRight)
+        Option(icon = Icons.Default.Info, text = "О приложении", buttonIcon = Icons.AutoMirrored.Filled.KeyboardArrowRight)
+        Option(icon = Icons.AutoMirrored.Filled.ExitToApp, text = "Выход", buttonIcon = Icons.AutoMirrored.Filled.KeyboardArrowRight)
+    }
+}
+
+@Preview
+@Composable
+fun Settings(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 8.dp, bottom = 4.dp)
+    ) {
+        Option(icon = Icons.Default.Face, text = "Язык", buttonIcon = Icons.Default.KeyboardArrowDown)
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Option(icon = Icons.Default.Build, text = "Темная тема", modifier = Modifier.weight(1f))
+            Switch(checked = false, onCheckedChange = {}, modifier = Modifier.size(40.dp, 20.dp).scale(0.7f))
+        }
     }
 }
 
@@ -99,23 +119,21 @@ fun ProfileInfo(modifier: Modifier = Modifier) {
 @Preview
 @Composable
 fun Option(
-    imageVector: ImageVector = Icons.Default.AccountCircle,
+    modifier: Modifier = Modifier,
+    icon: ImageVector = Icons.Default.AccountCircle,
     text: String = "random",
-    onClick: () -> Unit = {},
-    modifier: Modifier = Modifier
+    onClick: (() -> Unit)? = null,
+    buttonIcon: ImageVector? = null
 ) {
     Row(
         horizontalArrangement = Arrangement.spacedBy(16.dp),
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
             .fillMaxWidth()
-            .clickable(
-                interactionSource = remember { MutableInteractionSource() },
-                indication = null
-            ) { onClick() }
+            .then(if (onClick != null) Modifier.clickable { onClick() } else Modifier)
     ) {
         Icon(
-            imageVector = imageVector,
+            imageVector = icon,
             contentDescription = null,
             modifier = Modifier.size(20.dp)
         )
@@ -125,12 +143,28 @@ fun Option(
             fontSize = 14.sp,
             modifier = Modifier.weight(1f)
         )
-        Icon(
-            imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
-            contentDescription = null,
-            modifier = Modifier.size(24.dp)
-        )
+        if (buttonIcon != null) {
+            Icon(
+                imageVector = buttonIcon,
+                contentDescription = null,
+                modifier = Modifier.size(20.dp)
+            )
+        }
     }
+}
+
+@Preview
+@Composable
+fun About(modifier: Modifier = Modifier) {
+    Text(fontSize = 20.sp, fontWeight = FontWeight.Medium,
+        text = "Приложение позволяет быстро и удобно записываться на приём" +
+                " к врачам, оплачивать услуги онлайн и взаимодействовать с" +
+                " клиниками через встроенный чат.\n" +
+                "\uD83D\uDC68\u200D\uD83D\uDCBB Богдан Топорин\n" +
+                "\uD83D\uDC68\u200D\uD83D\uDCBB Холматов Темур\n" +
+                "\uD83D\uDC68\u200D\uD83D\uDCBB Алексей Утенков\n" +
+                "\uD83D\uDC69\u200D\uD83D\uDCBB Анна Гладышева\n" +
+                "\uD83D\uDC68\u200D\uD83D\uDCBB Александр Шубин")
 }
 
 @Preview
@@ -158,5 +192,17 @@ fun Avatar(modifier: Modifier = Modifier) {
             )
         }
     }
+}
+
+@Composable
+fun EditProfile(modifier: Modifier = Modifier) {
+
+    var username by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = username,
+        onValueChange = { username = it },
+        label = { Text("Username") }
+    )
 }
 
