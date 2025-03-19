f6a8cc682385d20c7d75df0fb864f3ac0497e35d
Александр Ш <C:\Users\alexa\AppData\Roaming\The Bat!>
2025-03-19 18:10:04 +0300

    ClinicScreen

--- app/src/main/java/com/CoolPeppers/android/presentation/service/ServiceScreen.kt
+++ app/src/main/java/com/CoolPeppers/android/presentation/service/ServiceScreen.kt
@@ -1,14 +1,112 @@
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.CoolPeppers.android.R
+import com.CoolPeppers.android.data.model.Service
+import com.CoolPeppers.android.ui.theme.LightTextPrimary
+import androidx. compose. foundation. lazy. grid. LazyVerticalGrid
+import androidx. compose. foundation. lazy. grid. GridCells
+
+
+import androidx.compose.foundation.lazy.grid.items
+
+
+val service1 = Service(
+    id = 1,
+    name = "Стоматология",
+    description = "Лечение зубов",
+    icon = R.drawable.service_blue,
+)
+val serviceList = List(10) { service1 }
 
 @Composable
 fun ServiceScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("Услуги", style = MaterialTheme.typography.headlineLarge)
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+                    .background(Color.White)
+                    .padding(horizontal = 5.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.service_blue),
+                    contentDescription = "List Icon",
+                    modifier = Modifier.size(20.dp)
+                )
+                Text(
+                    text = " Выберите услугу",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = LightTextPrimary
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(3),
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(8.dp)
+            ) {
+                items(serviceList) { service ->
+                    ServiceItem(service = service)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ServiceItem(service: Service) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .size(60.dp)
+                .background(Color(0xFF2F6690).copy(alpha = 0.08f), shape = RoundedCornerShape(50))
+        ) {
+            Image(
+                painter = painterResource(id = service.icon),
+                contentDescription = "Service Icon",
+                modifier = Modifier
+                    .size(30.dp)
+                    .align(Alignment.Center)
+            )
+        }
+        Text(
+            text = service.name,
+            color = Color(0xFF2F6690),
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier.padding(top = 8.dp)
+        )
     }
 }
