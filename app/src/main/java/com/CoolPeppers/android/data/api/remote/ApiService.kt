e37a0358585f098fae3ca655072d226818c09e66
Bogdan <ciborg123@yandex.ru>
2025-03-24 15:23:18 +0300

    fix api interface (services) and add reps

--- app/src/main/java/com/CoolPeppers/android/data/api/remote/ApiService.kt
+++ app/src/main/java/com/CoolPeppers/android/data/api/remote/ApiService.kt
@@ -79,7 +79,7 @@ interface  ApiService {
         @Query("skip") skip: Int = 0,
         @Query("limit") limit: Int = 100,
         @Query("search") search: String = "",
-        @Query(ApiConstants.SERVICE_ID) serviceId: Int?
+        @Query(ApiConstants.CLINIC_ID) clinicId: Int?
     ): List<Service>
 
     @GET(ApiConstants.GET_SERVICE_URL)
