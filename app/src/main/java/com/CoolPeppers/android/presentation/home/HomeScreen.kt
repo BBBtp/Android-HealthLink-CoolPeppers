dea1938f0bb215652990111920312d534ca480f2
Bogdan <ciborg123@yandex.ru>
2025-03-25 16:05:17 +0300

    create integration branch

--- app/src/main/java/com/CoolPeppers/android/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/CoolPeppers/android/presentation/home/HomeScreen.kt
@@ -1,6 +1,5 @@
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,34 +7,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.CoolPeppers.android.R
-import com.CoolPeppers.android.data.model.Clinic
 import com.CoolPeppers.android.presentation.clinic.ClinicViewModel
 import com.CoolPeppers.android.presentation.doctor.DoctorViewModel
 import com.CoolPeppers.android.presentation.home.components.ClinicCard
 import com.CoolPeppers.android.presentation.home.components.DoctorCard
-import com.CoolPeppers.android.presentation.home.components.RecordCard
 import com.CoolPeppers.android.presentation.home.components.UserInfoBlock
 import com.CoolPeppers.android.ui.theme.LightTextPrimary
 import com.CoolPeppers.android.ui.theme.Montserrat
-import dagger.hilt.android.AndroidEntryPoint
 
 
 @Composable
